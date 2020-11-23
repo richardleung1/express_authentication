7740171b65d9d8f1be9028632d3232d386bd169e
Richard Leung <michelleli@MMac.local>
2020-11-23 15:09:25 -0500

    Create middleware and add to server

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ const flash = require('connect-flash');
 const SECRET_SESSION = process.env.SECRET_SESSION;
 const app = express();
 
+const isLoggedIn = require('./middleware/isLoggedIn');
+
 app.set('view engine', 'ejs');
 
 app.use(require('morgan')('dev'));
